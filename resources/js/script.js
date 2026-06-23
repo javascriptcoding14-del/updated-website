becc12bf7fe94b73fd4efd461e81cf978c176612
REENA <javascriptcoding14@gmail.com>
2026-06-23 13:47:36 +0530

    Delete resources/js directory

--- resources/js/script.js
+++
@@ -1,5 +0,0 @@
-const humbergur=document.getElementById("humbergur");
-const navHumbergur=document.getElementById("nav-humbergur");
-humbergur.addEventListener("click",()=>{
-    navHumbergur.style.display="flex";
-})
